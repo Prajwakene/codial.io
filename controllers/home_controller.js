82ca4cf68f94ba047cb38d2e98d3b4ca3a00ea02
Prajwal Kene <127944958+Prajwakene@users.noreply.github.com>
2023-05-30 19:53:08 +0530

    set up static files access for layout

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,7 +1,13 @@
 // exporting a functon which is publically acess to my routes
 module.exports.home = function(req, res){
-
-return res.end('<h1>Expres is up for codial</h1>')
+    //rendering home.ejs
+    return res.render('home', {
+        title : "Home"
+    });
 }
+// return res.end('<h1>Expres is up for codial</h1>')
+// }
 
-module.express
+// module.express.actionName = function(req, res){
+
+// }
--- index.js
+++ index.js
@@ -5,12 +5,26 @@ const app = express();
 //when we deploy it to the server this port number will be 80 
 const port = 8000;
 
+// 5th step....accessing the express-ejs-layouts  library 
+const expressLayouts = require('express-ejs-layouts');
+
+//7th step...extract style and sripts from sub page in to the layout 
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+//6th step..setting up the static files
+// ./ tells in the neighbouring folder it willl loke for the folder name assets creating it
+app.use(express.static('./assets'));
+
+
+app.use(expressLayouts);
+
 //3rd step
 //use express router 
 app.use('/', require('./routes'));
 
 
-// 4th strp...setting up the view engine using app.set documentry
+// 4th step...setting up the view engine using app.set documentry
 app.set('view engine', 'ejs');
 // ./ means it tis inte neighbouring folder
 app.set('views', './views');
