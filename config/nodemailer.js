6218c569c50502f09bcd6f292c98ab11087b71c4
Prajwal Kene <127944958+Prajwakene@users.noreply.github.com>
2023-07-03 22:56:18 +0530

    sending our first mail

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,5 +1,10 @@
-// importing nodemailer
+// importing nodemailer ejs path as thjey require to set up the nodemailer
 const nodemailer = require('nodemailer');
+
+//importing ejs
+const ejs = require('ejs');
+//importing path
+const path = require(path);
 
 //defining transportar it is object willl attch it to the node mailer
 let transportar = nodemailer.createTransport({
@@ -14,3 +19,25 @@ let transportar = nodemailer.createTransport({
 });
 
 
+//..wee are telling that we are using the EJS for rendering HTML tenplate 
+let renderTemplate = (data, relativePath) => {
+    //defined variable where we are storing what all HTMl is going t be sent in that mail 
+    let mailHTML;
+    ejs.renderFile(
+        path.join(__dirname, '../views/mailers', relativePath),
+        data,
+        function(err, template){
+            if(err){
+                console.log('error in rendering template');
+                return;
+            }
+            mailHTML = template;
+        }
+    )
+    return mailHTML;
+}
+
+module.exports = {
+    transportar: transportar,
+    renderTemplate: renderTemplate
+}
